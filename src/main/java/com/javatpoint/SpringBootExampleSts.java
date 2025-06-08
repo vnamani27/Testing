1bbf19576e64766753552a7fec64fb11c314177b
vnamani <vyshnavinamani27@gmail.com>
2025-06-08 19:19:25 -0400

    test

---
+++ src/main/java/com/javatpoint/SpringBootExampleSts.java
@@ -0,0 +1,11 @@
+package com.javatpoint;
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+@SpringBootApplication 			////similar to @Configuration @EnableAutoConfiguration @ComponentScan 
+public class SpringBootExampleSts 
+{
+public static void main(String[] args)
+{  
+SpringApplication.run(SpringBootExampleSts.class, args);  
+} 
+}
